42e114ae2187bae5dd1f501b97a11cee5c64a5ae
danikdragon <danik.popov.02@gmail.com>
2024-02-25 17:01:17 +0600

    task 2

--- number 33/task 2/task 2/task 2.cpp
+++ number 33/task 2/task 2/task 2.cpp
@@ -5,14 +5,97 @@
 
 using namespace std;
 
-int main()
+class WinExcept : public exception
 {
-	int pool[3][3]{ 0 };
-	
+public:
+	const char* what() const noexcept override
+	{
+		return "you won";
+	}
+};
+
+class LoseExcept : public exception
+{
+public:
+	const char* what() const noexcept override
+	{
+		cout << "Try again: ";
+		return "you lose";
+	}
+};
+
+enum POOL
+{
+	NONE,
+	BOOT,
+	FISH
+};
+
+void foo(int pool[9])
+{
+	int select;
+	cout << "Select cell (1-9): ";
+	cin >> select;
+	select--;
+
+	if (select > 8 || select < 0)
+		throw invalid_argument("Select > 9 | < 1");
+	if (pool[select] == FISH)
+		throw WinExcept();
+	if (pool[select] == BOOT)
+		throw LoseExcept();
+	if (pool[select] == NONE)
+		throw invalid_argument("None");
 }
 
-//0 ничего 
-//1 рыба
-//2 ботинок
-// 
-// 33 номер
+int main()
+{
+	srand(time(nullptr));
+	int pool[9]{ 0 };
+
+	for (int i = 0; i < 3;)
+	{
+		int my_rand = rand() % 9;
+		if(pool[my_rand] == NONE)
+		{
+			i++;
+			pool[my_rand] = BOOT;
+			cout << "Boot number: " << i << " on pool position: " << my_rand+1 << "\n";
+		}
+	}
+
+	while (true)
+	{
+		int my_rand = rand() % 9;
+		if (pool[my_rand] == NONE)
+		{
+			pool[my_rand] = FISH;
+			cout << "Fish on pool position: " << my_rand + 1 << "\n";
+			break;
+		}
+	}
+
+	for (int i = 0; true;)
+	{
+		try
+		{
+			i++;
+			foo(pool);
+		}
+		catch(invalid_argument e)
+		{
+			i--;
+			cout << e.what() << "\n";
+		}
+		catch(WinExcept e)
+		{
+			cout << e.what() << "\n";
+			cout << "Try count " << i;
+			break;
+		}
+		catch (LoseExcept e)
+		{
+			cout << e.what() << "\n";
+		}
+	}
+}
