f955289802627befd82f056e449b1b3decf15efc
danikdragon <danik.popov.02@gmail.com>
2024-02-27 16:42:50 +0600

    task 30 complite
    выполнил 30 задание с cpr

--- 30 HTTP запросы cpr/task3/main.cpp
+++ 30 HTTP запросы cpr/task3/main.cpp
@@ -24,28 +24,32 @@ int main()
 		if (value == "get" || value == "post")
 			break;
 
-		arguments.insert(key,value);
+		arguments.insert({ key,value });
 	}
 
 	if(key == "get" || value == "get")
 	{
 		string getArgs = "?";
-		for (map<string,string>::iterator i = arguments.begin(); i != arguments.end(); i++)
-		{
-			getArgs += i->first + "=";
-			getArgs += i->second + "&";
-		}
-		r = cpr::Get(cpr::Url(pathGet), cpr::Header("args", getArgs.c_str()));
+		for (map<string, string>::iterator i = arguments.begin(); i != arguments.end(); i++)
+			getArgs += (i->first + "=") + (i->second + "&");
+
+		pathGet += getArgs;
+		r = cpr::Get(cpr::Url(pathGet));
+
 		cout << r.text << endl;
 	}
-	else if(key == "post" || value == "post")
+	else if (key == "post" || value == "post")
 	{
-		cpr::Payload payload{};
+		//cpr::Payload payload{ arguments.begin() ,arguments.end() };
+		//for (map<string, string>::iterator i = arguments.begin(); i != arguments.end(); i++)
+		//	payload.Add(cpr::Pair(i->first.c_str(), i->second.c_str()));
+
+		string getArgs = "";
 		for (map<string, string>::iterator i = arguments.begin(); i != arguments.end(); i++)
-		{
-			payload.Add(cpr::Pair((string)i->first.c_str(), (string)i->second.c_str()));
-		}
-		r = cpr::Post(cpr::Url(pathPost), cpr::Header("form", payload));
+			getArgs += (i->first + "=") + (i->second + "&");
+
+		r = cpr::Post(cpr::Url(pathPost),
+			cpr::Body(getArgs));
 		cout << r.text << endl;
 	}
 	return 0;
