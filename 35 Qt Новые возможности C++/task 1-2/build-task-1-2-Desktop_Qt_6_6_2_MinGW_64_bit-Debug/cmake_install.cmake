8cc87009d5d025ff06667addfc4906150d20437b
danikdragon <danik.popov.02@gmail.com>
2024-03-08 01:59:53 +0500

    36

---
+++ 35 Qt Новые возможности C++/task 1-2/build-task-1-2-Desktop_Qt_6_6_2_MinGW_64_bit-Debug/cmake_install.cmake
@@ -0,0 +1,59 @@
+# Install script for directory: B:/c++/SkillTusk/35 Qt Новые возможности C++/task 1-2/task-1-2
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/task-1-2")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "Debug")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "FALSE")
+endif()
+
+# Set default install directory permissions.
+if(NOT DEFINED CMAKE_OBJDUMP)
+  set(CMAKE_OBJDUMP "B:/Qt/Tools/mingw1120_64/bin/objdump.exe")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE EXECUTABLE FILES "B:/c++/SkillTusk/35 Qt Новые возможности C++/task 1-2/build-task-1-2-Desktop_Qt_6_6_2_MinGW_64_bit-Debug/task-1-2.exe")
+  if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/task-1-2.exe" AND
+     NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/task-1-2.exe")
+    if(CMAKE_INSTALL_DO_STRIP)
+      execute_process(COMMAND "B:/Qt/Tools/mingw1120_64/bin/strip.exe" "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/task-1-2.exe")
+    endif()
+  endif()
+endif()
+
+if(CMAKE_INSTALL_COMPONENT)
+  set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
+else()
+  set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
+endif()
+
+string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
+       "${CMAKE_INSTALL_MANIFEST_FILES}")
+file(WRITE "B:/c++/SkillTusk/35 Qt Новые возможности C++/task 1-2/build-task-1-2-Desktop_Qt_6_6_2_MinGW_64_bit-Debug/${CMAKE_INSTALL_MANIFEST}"
+     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
---
+++ 36 Виджеты Qt/task-1/main.cpp
@@ -0,0 +1,110 @@
+﻿//#include "QtWidgetsApplication1.h"
+#include <QtWidgets/QApplication>
+#include <QSlider>
+#include <QSpinBox>
+#include <QPushbutton>
+#include <QPixmap>
+#include <QPainter>
+#include <QPaintEvent>
+#include <QTimer>
+#include <QKeyEvent>
+#include <string>
+
+using namespace std;
+
+class ImageButton : public QPushButton
+{
+    Q_OBJECT
+public:
+    ImageButton() = default;
+    ImageButton(QWidget* parent);
+    void paintEvent(QPaintEvent* e) override;
+    QSize sizeHint() const override;
+    QSize minimumSizeHint() const override;
+    void keyPressEvent(QKeyEvent* e) override;
+public slots:
+    void setUp();
+    void setDown();
+private:
+    QPixmap mCurrentButton;
+    QPixmap mDownButton;
+    QPixmap mUpButton;
+    bool isDown = false;
+};
+
+
+ImageButton::ImageButton(QWidget* parent)
+{
+    setParent(parent);
+    setToolTip("Это кнопка");
+    setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
+    mDownButton = QPixmap("B:/c++/SkillTusk/36 Виджеты Qt/task-1/down.png");
+    mUpButton = QPixmap("B:/c++/SkillTusk/36 Виджеты Qt/task-1/up.png");
+    mCurrentButton = mUpButton;
+    setFixedSize(100, 100);
+    connect(this, &QPushButton::clicked, this, &ImageButton::setDown);
+}
+
+void ImageButton::paintEvent(QPaintEvent* e)
+{
+    QPainter p(this);
+    p.drawPixmap(e->rect(), mCurrentButton);
+}
+
+QSize ImageButton::sizeHint() const
+{
+    return QSize(100, 100);
+}
+QSize ImageButton::minimumSizeHint() const
+{
+    return sizeHint();
+}
+void ImageButton::keyPressEvent(QKeyEvent* e)
+{
+    setDown();
+}
+void ImageButton::setDown()
+{
+    mCurrentButton = mDownButton;
+    update();
+    QTimer::singleShot(100, this, &ImageButton::setUp);
+}
+void ImageButton::setUp()
+{
+    mCurrentButton = mUpButton;
+    update();
+}
+
+int main(int argc, char* argv[])
+{
+    QApplication a(argc, argv);
+    ImageButton blackButton;
+    blackButton.setFixedSize(100, 100);
+    blackButton.move(1000, 400);
+    blackButton.show();
+    a.exec();
+}
+
+//слайдер и ползунок
+//QWidget* window = new QWidget;
+//window->resize(300, 300);
+//auto* slider = new QSlider(Qt::Horizontal, window);
+//auto* spinbox = new QSpinBox(window);
+//
+//slider->setMinimum(0);
+//slider->setMaximum(100);
+//spinbox->setMinimum(0);
+//spinbox->setMaximum(100);
+//
+//QObject::connect(slider, &QSlider::valueChanged, spinbox, &QSpinBox::setValue);
+//QObject::connect(spinbox, &QSpinBox::valueChanged, slider, &QSlider::setValue);
+//
+//slider->resize(100, 20);
+//spinbox->resize(40, 40);
+//
+//slider->move(100, 50);
+//spinbox->move(200, 50);
+//
+//slider->show();
+//spinbox->show();
+//window->show();
