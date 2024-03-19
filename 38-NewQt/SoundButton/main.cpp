7a41854711ce6470a0d8f7c433d07c3432bb8c15
danikdragon <danik.popov.02@gmail.com>
2024-03-19 13:45:32 +0500

    Обновление звуковой кнопки

--- 38-NewQt/SoundButton/main.cpp
+++ 38-NewQt/SoundButton/main.cpp
@@ -1,30 +1,29 @@
 #include <QApplication>
 #include <QPushButton>
-#include <QMediaPlayer>
 #include <QMediaContent>
+#include <QMediaPlayer>
 #include <QStyleOptionButton>
 #include <QStyle>
 #include <QUrl>
-#include <QDir>
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
     QPushButton button;
-    QDir::currentPath();
     button.setStyleSheet("QPushButton {"
-                            "border-image: url(../up.png);"
+                            "border-image: url(../SoundButton/up.png);"
                          "}"
                          "QPushButton:pressed {"
-                            "border-image: url(../down.png);"
+                            "border-image: url(../SoundButton/down.png);"
                          "}");
     button.setFixedSize(100,100);
-    QObject::connect(&button, &QPushButton::clicked, [&app](){
-        QMediaPlayer player;
-        player.setMedia(QUrl::fromLocalFile(":/mp/bip.mp3"));
-        player.setVolume(50);
-        player.play();
 
+    QObject::connect(&button, &QPushButton::clicked, [](){
+        QMediaPlayer player;
+        player.setMedia(QUrl::fromLocalFile("../SoundButton/bip.mp3"));
+        player.setVolume(100);
+        player.play();
     });
+
     button.show();
     return QApplication::exec();
 }
